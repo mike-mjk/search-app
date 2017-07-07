9701b4efdc0caeb1be782869c360583ed5097b2a
mike-mjk <mikeavida@gmail.com>
2017-07-06 18:24:25 -0700

    setup redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 import App from './App';
 import './index.css';
 
+const logger = createLogger({});
+const createStoreWithMiddleware = applyMiddleware(thunk, logger)(createStore);
+const store = createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+	<Provider store={store}>
+  	<App />
+  </Provider>
+  , document.getElementById('root')
 );
