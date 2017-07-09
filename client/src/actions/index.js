03f9549119cffddf60ae6e6114159bc49a9b2faa
mike-mjk <mikeavida@gmail.com>
2017-07-09 15:16:43 -0700

    get array of oembed reponse objects

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import jsonp from 'jsonp';
 
 export const SEARCH_TWITTER = 'search_twitter';
 
@@ -11,9 +12,38 @@ export function searchTwitter(term) {
 	// }
 }
 
-export function oEmbedRequest(url) {
-	axios.get(url)
-	.then(response => {
-		console.log(response);
-	});
+//oEmbed request for single url
+function oEmbedRequest(url) {
+	return new Promise((resolve, reject) => {
+		jsonp(url, null, function(err, data) {
+			if (err) {
+				console.error(err.message);
+			} else {
+				resolve(data);
+			}
+		});
+	})
 }
+
+export function arrayOEmbedRequest(urls) {
+	let oEmbedResponseArray = []
+	for (let i = 0; i < urls.length; i++) {
+		oEmbedRequest(urls[i])
+		.then(response => {
+			oEmbedResponseArray.push(response);	
+		})
+	}
+	return oEmbedResponseArray;
+}
+
+
+// before trying to implement promise
+// function oEmbedRequest(url) {
+// 	jsonp(url, null, function(err, data) {
+// 		if (err) {
+// 			console.error(err.message);
+// 		} else {
+// 			return data;
+// 		}
+// 	});
+// }
--- client/src/index.js
+++ client/src/index.js
@@ -12,9 +12,20 @@ const logger = createLogger({});
 const createStoreWithMiddleware = applyMiddleware(thunk, logger)(createStore);
 const store = createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-import { searchTwitter, oEmbedRequest } from './actions/';
+import { searchTwitter, arrayOEmbedRequest } from './actions/';
 searchTwitter('trump');
-oEmbedRequest('https://publish.twitter.com/oembed?url=https://twitter.com/MarySadler19/status/884162950638043136')
+
+let myArr = arrayOEmbedRequest([ 'https://publish.twitter.com/oembed?url=https://twitter.com/thugsRbadMK/status/884171918798147584',
+  'https://publish.twitter.com/oembed?url=https://twitter.com/TylerHu99890202/status/884171918617698305',
+  'https://publish.twitter.com/oembed?url=https://twitter.com/marcylauren/status/884171918450069504',
+  'https://publish.twitter.com/oembed?url=https://twitter.com/AlbaneseJoe/status/884171918408069120',
+  'https://publish.twitter.com/oembed?url=https://twitter.com/cyndi_obrion/status/884171918299058178',
+  'https://publish.twitter.com/oembed?url=https://twitter.com/Anna_Tweeterowa/status/884171918299025409',
+  'https://publish.twitter.com/oembed?url=https://twitter.com/crisp_aw/status/884171918210936834',
+  'https://publish.twitter.com/oembed?url=https://twitter.com/SheilaPettifor2/status/884171918206791680',
+  'https://publish.twitter.com/oembed?url=https://twitter.com/KateWalter12/status/884171918198419461',
+  'https://publish.twitter.com/oembed?url=https://twitter.com/preferscleanH2O/status/884171918131183616' ]);
+console.log('myArr', myArr);
 
 ReactDOM.render(
 	<Provider store={store}>
